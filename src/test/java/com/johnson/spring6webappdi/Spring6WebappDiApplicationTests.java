5baaef6708715eb2f270330023196b2ba80b8dd4
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-07-18 08:22:08 +0200

    spring context test: - using inject into context - using inject Controller

--- src/test/java/com/johnson/spring6webappdi/Spring6WebappDiApplicationTests.java
+++ src/test/java/com/johnson/spring6webappdi/Spring6WebappDiApplicationTests.java
@@ -1,10 +1,36 @@
 package com.johnson.spring6webappdi;
 
+import com.johnson.spring6webappdi.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6WebappDiApplicationTests {
+
+	// Spring will inject the context into this variable.
+	@Autowired
+	ApplicationContext ctx;
+
+	// we are asking Spring to inject the controller into this variable.
+	@Autowired
+	MyController controller;
+
+	@Test
+	void testAutowiredOfController() {
+		System.out.println("--------");
+		System.out.println(controller.sayHello());
+	}
+
+	// Spring will inject the context into this variable.
+	@Test
+	void testGetControllerFromCtx() {
+		MyController controller = (MyController) ctx.getBean(MyController.class);
+
+		System.out.println("--------");
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
